f1a8b9a61fe4990d800ca4a0972be4466d65fba4
alexvassiliou <alex.vassiliou@ipcompute.com>
2019-06-20 16:59:05 +1000

    route to add author

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
@@ -69,8 +70,25 @@ function onAnswerSelected (answer) {
     render();
 }
 
+function AddAuthorForm({match}) {
+    return <div>
+        <h1>Add author</h1>
+        <p>{JSON.stringify(match)}</p>
+    </div>;
+}
+
+function App () {
+    return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />;
+}
+
 function render() {
-    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+    ReactDOM.render(
+        <BrowserRouter>
+            <React.Fragment>
+                <Route exact path="/" component={App} />
+                <Route path="/add" component={AddAuthorForm} />
+            </React.Fragment>
+        </BrowserRouter>, document.getElementById('root'));
 }
 render();
 
